6c486fd60ef2e85995df25939db28060efda7f15
dharmi9397 <patelzerox2008@gemail.com>
2024-01-01 16:33:12 +0530

    web devlopment

---
+++ NUMBER1.C
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <conio.h>
+main()
+{
+ int a,b;
+ clrscr();
+ printf("enter a number of first number :-");
+ scanf("%d",&a);
+ printf("enter a value of second number :-");
+ scanf("%d",&b);
+ if(a<b){
+  printf("%d",a);
+  }else{
+  printf("%d",b);
+  }
+ getch();
+
+}
